e51d589b0ecd3b426acd2815d657981eb951ff73
shinji hagio <shinjihagio@gmail.com>
2020-10-05 15:13:28 +0000

    learn state

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
 import React, { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return(
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onClick={() => {console.log("I am Clicked")}} />
-//       </React.Fragment>
-//       )
-//   }
-// }
-const App = () => {
-  const profiles = [
-    {
-      name: "Taro",
-      age: 10
-    },
-    {
-      name: "Hanako",
-      age: 8
-    },
-    {
-      name: "Chinatsu",
-      age: 2
-    },
-    {
-      name: "Shinji",
-    }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((value, index) => {
-          return <User name={value.name} age={value.age} key={index} />
-        })
-      }
-    </div>
-  )
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state) 
+    this.state = {count: 0}
+  }
+
+  handlePlusButton = () => {
+    this.setState(
+      { count: this.state.count + 1}
+    )
+  }
+
+  handleMinusButton = () => {
+    this.setState(
+      { count: this.state.count - 1}
+    )
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={ this.handlePlusButton }>+1</button>
+        <button onClick={ this.handleMinusButton }>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return (
-  <div>
-    Hi, I am {props.name}, {props.age} years old!
-  </div>)
-}
-
-User.defaultProps = {
-  age: 1
-}
 
 export default App;
